0e8574134488a0d88dfcc8b7cc8e5f2891fc1ec5
gauravprsnt <prsnt.gaurav@gmail.com>
2018-05-30 15:40:43 +0545

    latest Commit

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,9 +8,15 @@
  * Controller of the myAppApp
  */
 angular.module('myAppApp')
-  .controller('MainCtrl', function ($scope,AppService) {
-    $scope.data =[];
+  .controller('MainCtrl', function ($scope, AppService) {
+    $scope.btn = false;
+    $scope.btn11 = false;
+    $scope.msg= false;
+    $scope.edit=[];
+
+
     $scope.getStudent = function () {
+      $scope.data = [];
       AppService.findAllStudent().then(
         function (response) {
           console.log(response);
@@ -21,14 +27,97 @@ angular.module('myAppApp')
         }
       );
     };
-    $scope.insertStudent=function () {
-      $scope.insertdata={
-        name:'test',
-        address:'brt',
-        std:'degree'
+
+    $scope.insertStudent = function () {
+      $scope.data = [];
+      $scope.msg= true;
+      $scope.btn = false;
+      $scope.insertdata = {
+        name: $scope.abc,
+        address: $scope.def,
+        std: $scope.mayap
       };
-      AppService.insertStudentData(insertdata);
+      AppService.insertStudentData($scope.insertdata).then(
+        function (response) {
+          $scope.msg= true;
+          console.log(response);
+          $scope.data.push(response);
+          $scope.insertdata = {
+            name:'',
+            address:'',
+            std:''
+          };
+
+        },
+        function () {
+          console.log('Failure');
+        }
+      );
+
 
     };
+
+    $scope.searchOne = function () {
+      $scope.data = [];
+      $scope.btn11 = false;
+      AppService.findOneStudent($scope.studentId).then(
+        function (response) {
+          console.log(response);
+          $scope.data.push(response);
+        }
+      );
+      $scope.studentId = '';
+    };
+
+    $scope.updateStatus = function (studentId) {
+      $scope.data = [];
+      AppService.updateSingleStatus(studentId).then(
+        function (response) {
+          console.log(response);
+          $scope.data.push(response);
+        }
+
+      );
+    };
+
+
+    $scope.saveEditedData=function (index,student) {
+      console.log(student);
+      console.log(student.id);
+      // student={
+      //   id:student.id,
+      //   name:$scope.edname,
+      //   address:'efgh',
+      //   std:'adasdf',
+      //   status:student.status
+      // };
+      console.log(student);
+
+      AppService.updateStudent(student);
+      $scope.edit[index] =false;
+
+    };
+
+
+    $scope.bottonClicked = function () {
+      $scope.btn = true;
+    };
+
+    $scope.bottonClickedOne = function () {
+      $scope.btn11 = true;
+    };
+
+    $scope.triggerEditMode = function (index) {
+      $scope.edit[index] =true;
+    };
+
+    $scope.removeEditMode = function (index) {
+      $scope.edit[index] =false;
+    };
+
+    $scope.disbleAdd=function () {
+      $scope.btn=false;
+    }
+
   });
 
--- app/scripts/services/AppService.js
+++ app/scripts/services/AppService.js
@@ -1,16 +1,33 @@
 'use strict';
 
 angular.module('myAppApp')
-  .service('AppService',['Restangular',
+  .service('AppService', ['Restangular',
     function (Restangular) {
-     var student=Restangular.all('students');
+      var student = Restangular.all('students');
       return {
         findAllStudent: function () {
           return student.getList();
         },
 
         insertStudentData: function (insertdata) {
-         student.post(insertdata);
+          return student.post(insertdata);
+
+        },
+
+        findOneStudent: function (studentId) {
+          return Restangular.one('students', studentId).get();
+        },
+
+        updateSingleStatus: function (studentId) {
+          return Restangular.one('students', studentId).one('delete').put();
+
+        },
+
+        updateStudent:function (student) {
+          console.log(student);
+          console.log(student.id);
+          var url=Restangular.one('students',student.id);
+          url.customPUT(student);
         }
 
       };
